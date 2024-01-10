3958fecfd98b9011fb74daee3db6cc6c3768216d
shanachan <77118749+kap320@users.noreply.github.com>
2024-01-10 13:13:04 +0700

    Update Genre.tsx

--- src/pages/Genre.tsx
+++ src/pages/Genre.tsx
@@ -26,7 +26,7 @@ const Genre = () => {
             behavior: "smooth",
          });
 
-         document.title = `Wajik Streaming | Genre : ${
+         document.title = `ranimev1 Streaming | Genre : ${
             slug
                ? slug.charAt(0).toUpperCase() + slug.slice(1).replace("-", " ")
                : ""
